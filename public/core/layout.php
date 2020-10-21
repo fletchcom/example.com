7920760c9f9403cc799886a71108fd4cde50a248
laurafletcher <laura@fletchcom.net>
2020-10-21 15:04:12 -0500

    Adjusted CSS

--- public/core/layout.php
+++ public/core/layout.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <title>About Laura Fletcher</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link href="../dist/css/main.min.css" type="text/css" rel="stylesheet">
-  </head>
-  <body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello, I am Laura Fletcher</title>
+    <link href="./dist/css/main.min.css" type="text/css" rel="stylesheet">
+</head>
+<body>
 
   <header>
         <span class="logo">My Web Site</span>
